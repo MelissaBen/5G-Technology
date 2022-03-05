9a13a7772ed1cafa88ae25988098a2d598b96efc
Melissa Ben <melissabenmeziane01@gmail.com>
2022-03-05 18:39:03 +0100

    Fixbug

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,25 +7,3 @@ $('.moreless-button').click(function() {
     $(this).text("Read more")
   }
 });
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBs4iHoDFsC-SoaBQrsupgt3OjDzk3ViOw",
-  authDomain: "fifth-generation-technology.firebaseapp.com",
-  projectId: "fifth-generation-technology",
-  storageBucket: "fifth-generation-technology.appspot.com",
-  messagingSenderId: "123695602458",
-  appId: "1:123695602458:web:04f024795e5f6ee4a32aa3",
-  measurementId: "G-GLMTD5W6BX"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
